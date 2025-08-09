b90c23358171efd45cb19ef6b5d70faaf44e7bf2
hellzflare <safanashanonpeermohamed@outlook.com>
2025-08-09 19:03:44 +0100

    imported functions from other files and fixed answer variable declaration

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from puzzles import logical_puzzles, insight_puzzles
+from ai_agent import solve_puzzle
+from human_interface import ask_puzzle
+
 def main():
     print("Welcome to AI Limits simulator")
     human_scores={"logic":0,"insight":0}
+    ai_scores = {"logic": 0, "insight": 0}
     total_logic=len(logical_puzzles)
     total_insight=len(insight_puzzles)
     #human attempt at the logical puzzles
@@ -15,3 +20,17 @@ def main():
         correct, _ =ask_puzzle(i)
         if correct:
             human_scores["insight"]+=1
+    #AI attempt at the logical puzzles
+    print("AI Logical Puzzles")
+    for i in logical_puzzles:
+        answer,correct=ask_puzzle(i)
+        print(f"AI answers puzzle {i['id']} with '{answer}' -> {'Correct' if correct else 'Incorrect'}")
+        if correct:
+            ai_scores["logic"]+=1
+    #AI attempt at insight puzzles
+    print("AI Insight Puzzles")
+    for i in insight_puzzles:
+        answer,correct=ask_puzzle(i)
+        print(f"AI guesses puzzle {i['id']} with '{answer}' -> {'Correct' if correct else 'Incorrect'}")
+        if correct:
+            ai_scores["insight"]+=1
