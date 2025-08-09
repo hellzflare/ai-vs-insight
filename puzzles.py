161bea580e4ae05d594ca83c97d95ce0561517c6
hellzflare <safanashanonpeermohamed@outlook.com>
2025-08-09 18:26:27 +0100

    Added insight puzzles

--- puzzles.py
+++ puzzles.py
@@ -1,4 +1,14 @@
+#making puzzles with lists and dictionaries
 logical_puzzles=[{"id":1,"question":"What is the next number in the sequence: 2,4,8,16 ?","answer":32,"type":"logic"},
                  {"id":2,"question":"If all bloops are razzies and all razzies are lazzies are all bloops definently lazzies? (yes/no)","answer":"yes","type":"logic"},
                  {"id":3,"question":"Complete the pattern:A,C,F,J,O?","answer":"U","type":"logic"},
                  {"id":4,"question":"True or false:Every prime number greater than 2 is odd","answer":"true","type":"logic"}]
+insight_puzzles=[{"id":101,"question":("A man walks into a room with 3 light switches.Only one controls a light bulb in the next room"
+                                       "He can only enter the room once.How does he find the correct switch?"),"answer":(
+                                        "Turn on one switch,wait,turn it off,turn another on,then enter the room"
+                                        "to check the bulb and warmth"
+                                       ),"type":"insight"},
+                 {"id":102,"question":"What can travel around the world while staying in the same spot?","answer":"A stamp","type":"insight"},
+                 {"id":103,"question":("I speak without a mouth and hear without ears.I have nobody,but I come alive with wind. What am I?"),"answer":"An echo","type":"insight"},
+                 {"id":104,"question":"You see me once in June,twice in Novemeber,but not at all in May.What am I?","answer":"The letter 'e","type":"insight"}]
+                                
