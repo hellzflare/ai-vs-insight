6c1b2bd4913164cd28c734ded615552ac31238ae
hellzflare <safanashanonpeermohamed@outlook.com>
2025-08-09 18:50:52 +0100

    Added scoring for human logic and insight puzzles

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 def main():
-    print("Welcome to the AI Limits simulator!")
+    print("Welcome to AI Limits simulator")
+    human_scores={"logic":0,"insight":0}
+    total_logic=len(logical_puzzles)
+    total_insight=len(insight_puzzles)
+    #human attempt at the logical puzzles
+    print("Human Logical Puzzles")
+    for i in logical_puzzles:
+        correct, _=ask_puzzle(i)
+        if correct:
+            human_scores["logic"]+=1
+    #human attempt at the insight puzzles
+    print("Human Insight Puzzles")
+    for i in insight_puzzles:
+        correct, _ =ask_puzzle(i)
+        if correct:
+            human_scores["insight"]+=1
